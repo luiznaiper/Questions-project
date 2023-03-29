3152770c99d1f9153663eef273a820c73ee6f3ad
luiznaiper <luiz_naiper@hotmail.com>
2023-03-29 07:20:26 -0300

    :construction: Delete useState in questions and data

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,12 @@ import data from './data'
 import Questions from './Questions'
 
 function App() {
-  const [questions, setQuestions] = useState(data)
+  const questions = data
   const [activeId, setActiveId] = useState(null)
 
   const toggleQuestion = (id) => {
     const newActiveId = id === activeId ? null : id
-    setActiveId(newActiveId) 
+    setActiveId(newActiveId)
   }
 
   return (
