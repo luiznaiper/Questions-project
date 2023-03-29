7fdc310a5cd632fe2ce0e4774f9b65b66b1226b8
luiznaiper <luiz_naiper@hotmail.com>
2023-03-29 07:19:57 -0300

    :construction: Finished toggle accordeon project

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
+import { useState } from 'react'
 import data from './data'
 import Questions from './Questions'
 
 function App() {
-  const questions = data
+  const [questions, setQuestions] = useState(data)
+  const [activeId, setActiveId] = useState(null)
+
+  const toggleQuestion = (id) => {
+    const newActiveId = id === activeId ? null : id
+    setActiveId(newActiveId) 
+  }
 
   return (
     <main>
-      <Questions questions={questions} />
+      <Questions
+        questions={questions}
+        activeId={activeId}
+        toggleQuestion={toggleQuestion}
+      />
     </main>
   )
 }
