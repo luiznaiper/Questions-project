d47d5e040f65fd89eba3cc12bf2c01978a18c6fd
luiznaiper <luiz_naiper@hotmail.com>
2023-03-29 07:05:57 -0300

    :construction: Add accordeon project

--- src/SingleQuestion.jsx
+++ src/SingleQuestion.jsx
@@ -4,12 +4,19 @@ import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai'
 const SingleQuestion = ({ title, info }) => {
   const [showInfo, setShowInfo] = useState(false)
 
+  const handleClick = () => {
+    setShowInfo(!showInfo)
+  }
+
   return (
     <article className="question">
       <header>
         <h5>{title}</h5>
+        <button className="question-btn" onClick={handleClick}>
+          {showInfo ? <AiOutlineMinus /> : <AiOutlinePlus />} 
+        </button>
       </header>
-      <p>{info}</p>
+      {showInfo && <p>{info}</p>}
     </article>
   )
 }
